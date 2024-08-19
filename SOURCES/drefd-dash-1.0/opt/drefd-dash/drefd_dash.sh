60cef8b5c44c0a0b15f869daa4d15e90afa0ca11
candrist <elderandrist@hotmail.com>
2024-08-19 11:12:02 -0600

    Update drefd_dash.sh

--- SOURCES/drefd-dash-1.0/opt/drefd-dash/drefd_dash.sh
+++ SOURCES/drefd-dash-1.0/opt/drefd-dash/drefd_dash.sh
@@ -6,7 +6,10 @@ modules=( "A" "B" "C" "D" "E" )
 
 while true; do
   for mod in ${modules[@]}; do
-    grep "${DREFD_ID} ${mod}" /var/log/drefd/drefd.log | sort -r | awk -v mod="$mod" -F"[:-]" '{printf "%s %s %s:%s:%s %s\n", mod, $1, $2, $3, $4, $7}' | uniq | jq -R -n '[inputs | split(" ") | {module: .[0], date: .[1], time: .[2], callsign: .[3]}]' > /dstar/html/lastheard/assets/json/${mod,,}.json
+    grep "${DREFD_ID} ${mod}" ${DREFD_LOG_PATH} | sort -r | awk -v mod="$mod" -F"[:-]" '{printf "%s %s %s:%s:%s %s\n", mod, $1, $2, $3, $4, $7}' | uniq | jq -R -n '[inputs | split(" ") | {module: .[0], date: .[1], time: .[2], callsign: .[3]}]' > ${DREFD_JSON_PATH}/${mod,,}.json
+    chown -R apache:apache ${DREFD_JSON_PATH}/${mod,,}.json
+    chmod -R 755 ${DREFD_JSON_PATH}/${mod,,}.json
+    chcon -R -t httpd_sys_content_t ${DREFD_JSON_PATH}/${mod,,}.json
   done
   sleep 5
 done
